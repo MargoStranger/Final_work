95f4204528bec48317d0b84b63014be9c6f8113f
MargoStranger <margosebastian13@gmail.com>
2023-07-28 22:15:47 +0300

    готов код

--- Finaly_code/Program.cs
+++ Finaly_code/Program.cs
@@ -4,32 +4,53 @@
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 Примеры:
-[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”] - strind - [st[1]] 
+["1234", "1567", "-2", "computer science"] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → [] */
 
-string[] Array = {"Hello", "2", "world", ":-)"};
-Console.Write($"Массив с элементами длиной меньше или равной 3: ");
-Array_Output(FindElements(Array));
+string[] Array = {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+int ArrayLength = Array.Length;
+
+Console.Write($"Массив с элементами длиной меньше или равной 3: [");
+Array_Output(FindElements(Array, ArrayLength));
 
 
-string[] FindElements(string Array)
+//Метод поиска длины массива
+int Get_Count_Elements (string[] Array, int size)
 {
-    string New_Array = new string (Array.Length);
-    for (int i = 0; i < Array.Length; i++)
+    int Count_Elements = 0;
+    for (int i = 0; i < size; i++)
     {
         if (Array[i].Length <= 3)
         {
-            New_Array[i] = Array[i];
+            Count_Elements += 1;
+        }
+    }
+    return Count_Elements;
+}
+
+//Метод  поиска элементов
+string[] FindElements(string[] Array, int size)
+{
+    string[] New_Array = new string [Get_Count_Elements(Array, size)];
+    int j = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            New_Array[j] = Array[i];
+            j += 1;
         }
     }
     return New_Array;
 }
 
-void Array_Output (string array)
+//Метод вывода массива
+void Array_Output (string[] array)
 {
-    for (int i = 0; i < array.length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($" {array[i]},");
+        Console.Write($"{array[i]}; ");
     }
+    Console.Write("]");
 }
